ac33d5d42dcdbce31a84e659b34618e0043418d0
ggg <gggg@gggdeMacBook-Pro.local>
2018-06-21 20:03:42 +0800

    step 4

--- app/settings.py
+++ app/settings.py
@@ -1,4 +1,9 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI ="mysql://root:66166161@localhost:3306/demo?charset=utf8"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
